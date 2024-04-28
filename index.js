431381c6f01b4e0ee6b900e1980f54ccfc1bf653
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-04-28 18:02:49 +0300

    Feat(Questions API): create GET questions API

--- index.js
+++ index.js
@@ -6,14 +6,22 @@ const port = 3000;
 app.use(express.json());
 
 // Endpoint to get questions
-app.get('/questions', (req, res) => {
-    pool.query('SELECT * FROM Questions', (err, results) => {
-        if (err) {
-            return res.status(500).json({ error: 'Failed to retrieve questions' });
-        }
-        res.json(results);
-    });
-});
+app.get('/questions', async (req, res) => {
+    const limit =  100;
+  try {
+    const [questions] =  await pool.query('SELECT * FROM Questions ORDER BY RAND() LIMIT ?',[limit]);
+    
+    let questionArray = [];
+
+    for(const question of questions) {
+      const [choices] =  await pool.execute('SELECT choice_id, choice_text FROM Choices WHERE question_id = ? LIMIT 3' , [question.question_id]);
+      questionArray.push({question: {id:question.question_id,text:question.question_text,choices: choices}});
+    }
+    res.send(questionArray);
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to fetch questions' })
+  }
+})
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
