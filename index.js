eebda7f0acfe800b2b9950a7a4c86b8ca49d65d5
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-04-29 12:52:20 +0300

    add cors and edit returned questions array

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const pool = require('./database'); // Import your database configuration
 const app = express();
 const port = 3000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 // Endpoint to get questions
-app.get('/questions', async (req, res) => {
+app.get('/api/v1/questions', async (req, res) => {
     const limit =  100;
   try {
     const [questions] =  await pool.query('SELECT * FROM Questions ORDER BY RAND() LIMIT ?',[limit]);
@@ -15,7 +17,7 @@ app.get('/questions', async (req, res) => {
 
     for(const question of questions) {
       const [choices] =  await pool.execute('SELECT choice_id, choice_text FROM Choices WHERE question_id = ? LIMIT 3' , [question.question_id]);
-      questionArray.push({question: {id:question.question_id,text:question.question_text,choices: choices}});
+      questionArray.push( {id:question.question_id,text:question.question_text,choices: choices});
     }
     res.send(questionArray);
   } catch (error) {
