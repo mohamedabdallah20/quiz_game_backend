eee6aea5f702d3147fe44257e9905866aa1b33fc
mohamedabdallah20 <mohamedabdala1442000@gmail.com>
2024-04-29 14:11:31 +0300

    Feat(error handling): add function to handle mysql error

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,11 +1,16 @@
 const pool = require('../db/database'); // Import your database configuration
+const { handleMySQLError } = require('../utils/mysqlErrorHandling');
 
 createUser = async (req, res) => {
-   try {
-    // res.send(req.body);
-   } catch (error) {
-    
-   }
+    try {
+        const [result] = await pool.execute(
+            'INSERT INTO Users (username, email) VALUES (?, ?)',
+            [req.body.name, req.body.email]
+        );
+        res.send({ success: true, message: 'User created successfully', userId: result.insertId });
+    } catch (error) {
+        handleMySQLError(error, res);
+    }
 }
 
 module.exports = {
---
+++ src/utils/mysqlErrorHandling.js
@@ -0,0 +1,19 @@
+function handleMySQLError(error, res) {
+    switch (error.code) {
+        case 'ER_DUP_ENTRY':
+            res.status(409).json({ success: false, message: 'Entry already exists', details: error.sqlMessage });
+            break;
+        case 'ER_BAD_TABLE_ERROR':
+        case 'ER_NO_SUCH_TABLE':
+            res.status(404).json({ success: false, message: 'Resource not found', details: error.sqlMessage });
+            break;
+        case 'ER_DATA_TOO_LONG':
+            res.status(413).json({ success: false, message: 'Data too long for column', details: error.sqlMessage });
+            break;
+        default:
+            // console.error('Database error:', error);
+            res.status(500).json({ success: false, message: 'There is a problem', details: error.sqlMessage });
+    }
+}
+
+module.exports = {handleMySQLError}
